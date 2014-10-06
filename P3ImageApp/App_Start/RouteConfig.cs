933151bf54f49b41c5ff340324d28901f1785a62
gabrielmoura23 <gabrielandrade.moura@gmail.com>
2014-10-06 14:33:37 -0300

    P3Image v6
    Correção da rota e algumas alterações.

--- P3ImageApp/App_Start/RouteConfig.cs
+++ P3ImageApp/App_Start/RouteConfig.cs
@@ -14,36 +14,19 @@ namespace P3ImageApp
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-            name: "TesteSlug1",
-            url: "teste/{slugcategoria}",
-            defaults: new { controller = "Formulario", action = "Categoria", slugcategoria = "" }
-            );
-
-            routes.MapRoute(
-            name: "TesteSlug2",
-            url: "teste/{slugcategoria}/{slugsubcategoria}",
-            defaults: new { controller = "Formulario", action = "SubCategoria", slugcategoria = "", slugsubcategoria = "" }
-            );
-
-            routes.MapRoute(
-            name: "TesteSlug3",
-            url: "teste/{slugcategoria}/{slugsubcategoria}/{desccampo}",
-            defaults: new { controller = "Formulario", action = "Campo", slugcategoria = "", slugsubcategoria = "", desccampo = "" }
-            );
-
-            routes.MapRoute(
-            name: "TesteSlug21",
-            url: "teste1/{slugcategoria}/{slugsubcategoria}",
-            defaults: new { controller = "SubCategoria", action = "Dinamico", slugcategoria = "", slugsubcategoria = "" }
-            );
-
-            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                constraints: new { controller = "Home|Campo|Formulario|Autenticacao|Categoria|Erro|Interno|SubCategoria" }
             );
 
-            
+            routes.MapRoute(
+            name: "TesteSlug",
+            url: "{slugcategoria}/{slugsubcategoria}",
+            defaults: new { controller = "SubCategoria", action = "Dinamico", slugcategoria = "", slugsubcategoria = "" },
+            constraints: new { controller = "SubCategoria", action = "Dinamico" }
+            );
+
         }
     }
 }
